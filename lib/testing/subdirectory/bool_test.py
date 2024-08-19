cbef5f081e1837d0f5e2d705f1c2ec10cc19ebc3
Kelvin Masambu <kevohmasambu@gmail.com>
2024-08-19 20:33:08 +0300

    done

--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -3,5 +3,5 @@
 from bool_functions import return_true
 
 def test_return_true():
-    '''in bool_functions, function "return_true" returns True.'''
-    assert return_true() == True
+    '''in bool_functions, function "return_true" returns False.'''
+    assert return_true() == False
